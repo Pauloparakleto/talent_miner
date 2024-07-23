b5312dcc57f15b9fff2e77e32e588df69dd27ff3
Paulo Felipe Souza <alparakleto@gmail.com>
2024-07-23 11:42:31 -0300

    feat submission show

--- app/controllers/v1/public/submissions_controller.rb
+++ app/controllers/v1/public/submissions_controller.rb
@@ -1,6 +1,24 @@
 class V1::Public::SubmissionsController < ApplicationController
+  INITIAL_PAGE = 1
+   
+  before_action :set_submission, only: [:show]
+
   def index
-    @submissions = Public::Submission.all
+    @submissions = Public::Submission.order(:created_at).page params.fetch(:page, INITIAL_PAGE)
     render :index, status: :ok
+  end
+
+  def show
+    render :show
+  end
+
+  def create
+    byebug
+  end
+
+  private
+
+  def set_submission
+    @submission = Public::Submission.find(params[:id])
   end
 end
--- spec/requests/v1/public/submissions_spec.rb
+++ spec/requests/v1/public/submissions_spec.rb
@@ -20,13 +20,67 @@ RSpec.describe 'v1/public/submissions', type: :request do
 
   let(:valid_headers) { {} }
 
-  describe 'GET /v1/public/submissions/' do
+  describe 'GET /index' do
     include_context 'with Public::Submission'
 
     it 'renders a sucessfull response' do
       get v1_public_submissions_path, headers: valid_headers, as: :json
-      byebug
       expect(response.status).to eq(200)
+    end
+  end
+
+  describe "GET /show" do
+    include_context 'with Public::Submission'
+
+    it "renders a successful response" do
+      get v1_public_submission_path(submission), as: :json
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Public::Submission" do
+        expect {
+          perform_enqueued_jobs do
+            post v1_public_submissions_path,
+               params: { submission: valid_attributes }, headers: valid_headers, as: :json
+          end
+        }.to change(Public::Submission, :count).by(1)
+      end
+
+      it "renders a JSON response with the new submission" do
+        post v1_public_submission_path,
+             params: { submission: valid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to match(a_string_including("application/json"))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Public::Submission" do
+        expect {
+          post v1_public_submission_path,
+               params: { submission: invalid_attributes }, as: :json
+        }.to change(Public::Submission, :count).by(0)
+      end
+
+      it "renders a JSON response with errors for the new submission" do
+        post v1_public_submissions_path,
+             params: { submission: invalid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to match(a_string_including("application/json"))
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    include_context 'with Public::Submission'
+
+    it "destroys the requested Submission" do
+      expect {
+        delete v1_public_submission_path(submissions), headers: valid_headers, as: :json
+      }.to change(Public::Submission, :count).by(-1)
     end
   end
 end
