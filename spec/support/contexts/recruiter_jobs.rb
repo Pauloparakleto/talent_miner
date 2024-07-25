b9a8f4687c733120a312d40aabdf3163363aaf2b
Paulo Felipe Souza <alparakleto@gmail.com>
2024-07-25 00:18:02 -0300

    fix null bypes Faker against Postgresql

--- spec/support/contexts/recruiter_jobs.rb
+++ spec/support/contexts/recruiter_jobs.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 shared_context 'with Recruiter::Job' do
-  let!(:recruiter) { Recruiter.create(recruiter_valid_attributes) }
+  let(:recruiter) { Recruiter.create(recruiter_valid_attributes) }
   let(:recruiter_valid_attributes) {
     {
       name: Faker::Name.name,
       email: Faker::Internet.unique.email,
-      password: Faker::String.random(length: 7)
+      password: Faker::Lorem.word
     }
   }
 
